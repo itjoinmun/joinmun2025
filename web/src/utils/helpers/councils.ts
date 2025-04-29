834116ebc56b65ce6adb03bf1a29cb602d912503
Andre <andhiki28@gmail.com>
2025-04-29 22:56:13 +0700

    feat(councils): add councils logo and update COUNCIL Details

--- web/src/utils/helpers/councils.ts
+++ web/src/utils/helpers/councils.ts
@@ -9,30 +9,60 @@ export type Council = {
 
 export const COUNCILS = [
   {
-    slug: "unsc",
-    name: "UNSC",
-    fullname: "United Nations Security Council",
-    src: "unsc.webp",
-    quote: "Reviving Collective Security: Refroming the UNSC to Address Modern Threats",
-    description:
-      "The UNSC is responsible for maintaining international peace and security. It has the authority to impose sanctions, authorize military action, and establish peacekeeping missions.",
-  },
-  {
-    slug: "ilo",
-    name: "ILO",
-    fullname: "International Labour Organization",
-    src: "ilo.webp",
-    quote: "Reviving Collective Security: Refroming the UNSC to Address Modern Threats",
-    description:
-      "The UNSC is responsible for maintaining international peace and security. It has the authority to impose sanctions, authorize military action, and establish peacekeeping missions.",
-  },
-  {
     slug: "un-women",
     name: "UN Women",
     fullname: "Equality and the Empwerment of Women",
-    src: "unsc.webp",
+    src: "UN Women.png",
+    quote: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+    description: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+  },
+  {
+    slug: "who",
+    name: "WHO",
+    fullname: "World Health Organization",
+    src: "WHO.png",
+    quote: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+    description: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+  },
+  {
+    slug: "ecofin",
+    name: "ECOFIN",
+    fullname: "Economic and Financial Committee",
+    src: "ECOFIN.png",
+    quote: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+    description: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+  },
+  {
+    slug: "unsc",
+    name: "UNSC",
+    fullname: "United Nations Security Council",
+    src: "UN.png",
     quote: "Reviving Collective Security: Refroming the UNSC to Address Modern Threats",
     description:
       "The UNSC is responsible for maintaining international peace and security. It has the authority to impose sanctions, authorize military action, and establish peacekeeping missions.",
+  },
+  {
+    slug: "crisis",
+    name: "Crisis Committee",
+    fullname: "Crisis Committee",
+    src: "UN.png",
+    quote: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+    description: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+  },
+  {
+    slug: "brics",
+    name: "BRICS+",
+    fullname: "BRICS+",
+    src: "BRICS+.png",
+    quote: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+    description: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+  },
+  {
+    slug: 'press',
+    name: 'Press Corps',
+    fullname: 'Press Corps',
+    src: 'UN.png',
+    quote: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+    description: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
   },
 ];
