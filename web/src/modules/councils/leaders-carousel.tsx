a205af24fde7ea54129a6d5e6fdba39a1eca9d45
Andre <andhiki28@gmail.com>
2025-04-30 20:21:54 +0700

    feat(council page): add carousel leader profile

--- web/src/modules/councils/leaders-carousel.tsx
+++ web/src/modules/councils/leaders-carousel.tsx
@@ -1,5 +1,15 @@
 "use client";
 import {
+  Dialog,
+  DialogContent,
+  DialogOverlay,
+  DialogTrigger,
+  DialogTitle,
+  DialogHeader,
+  DialogDescription,
+  DialogFooter,
+} from "@/components/ui/dialog";
+import {
   Carousel,
   CarouselContent,
   CarouselItem,
@@ -7,6 +17,7 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import { Council } from "@/utils/helpers/councils";
+import { ArrowUpRightIcon, X } from "lucide-react";
 import Image from "next/image";
 
 export const LeaderCarousel = (props: Council) => {
@@ -19,13 +30,13 @@ export const LeaderCarousel = (props: Council) => {
     >
       <CarouselContent className="">
         {props.chairs.map((chair, index) => (
-          <CarouselItem key={index} className="basis-1/2">
-        <LeaderCard {...chair} />
+          <CarouselItem key={index} className="xs:basis-1/2">
+            <LeaderCard {...chair} />
           </CarouselItem>
         ))}
       </CarouselContent>
       <CarouselPrevious className="block border" />
-      <CarouselNext className="right-2 z-10 block border" />
+      <CarouselNext className="z-10 block border" />
     </Carousel>
   );
 };
@@ -42,39 +53,90 @@ const LeaderCard = ({
   experiences: string[];
 }) => {
   return (
-    <div className="relative flex h-56 gap-2 rounded-lg bg-black/50 lg:bg-transparent">
-      {/* Image Section */}
-      <div className="relative h-full w-full overflow-hidden rounded-md lg:w-1/3">
-        <Image
-          // src={`/assets/councils/${image}`}
-          src={`/lebron.webp`}
-          alt={`${name}'s Image`}
-          fill
-          sizes="33%"
-          className="pointer-events-none object-cover"
-        />
-        <div className="absolute bottom-4 left-4 lg:bottom-2 lg:left-2 z-10">
-          <h1 className="text-lg font-bold text-white lg:hidden">{role}</h1>
-          <h1 className="font-medium text-white lg:text-lg lg:font-bold">{name}</h1>
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="pointer-events-auto relative flex h-56 gap-2 rounded-lg bg-black/50 lg:pointer-events-none lg:bg-transparent">
+          {/* Image Section */}
+          <div className="relative h-full w-full overflow-hidden rounded-md lg:w-1/3">
+            <div className="bg-gray/80 hover:bg-gray/60 absolute top-4 right-4 z-10 cursor-pointer rounded-full p-2 lg:hidden">
+              <ArrowUpRightIcon color="white" />
+            </div>
+
+            <Image
+              // src={`/assets/councils/${image}`}
+              src={`/lebron.webp`}
+              alt={`${name}'s Image`}
+              fill
+              sizes="33%"
+              className="pointer-events-none object-cover"
+            />
+            <div className="absolute bottom-4 left-4 z-10 lg:bottom-2 lg:left-2">
+              <h1 className="text-lg font-bold text-white lg:hidden">{role}</h1>
+              <h1 className="font-medium text-white lg:text-lg lg:font-bold">{name}</h1>
+            </div>
+            <div className="bg-gradient-from-transparent absolute inset-x-0 bottom-0 h-20 bg-gradient-to-t from-black to-transparent" />
+          </div>
+
+          {/* Role & Experience Section */}
+          <div className="bg-gray hidden w-2/3 flex-col justify-start rounded-md p-4 lg:flex">
+            <h2 className="text-lg font-bold text-white">{role}</h2>
+
+            <hr className="border-gray-light my-2 w-full border-b" />
+
+            <h3 className="text-sm">Past Experience</h3>
+            <ul className="mt-2 flex flex-wrap gap-2 text-xs text-white">
+              {experiences.map((experience, index) => (
+                <li
+                  key={index}
+                  className="rounded-full border border-neutral-500 bg-neutral-700/50 px-2.5 py-1 text-nowrap"
+                >
+                  {experience}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-        <div className="bg-gradient-from-transparent absolute inset-x-0 bottom-0 h-20 bg-gradient-to-t from-black to-transparent" />
-      </div>
+      </DialogTrigger>
 
-      {/* Role & Experience Section */}
-      <div className="bg-gray hidden w-2/3 flex-col justify-start rounded-md p-4 lg:flex">
-        <h2 className="text-lg font-bold text-white">{role}</h2>
+      <DialogContent className="2xs:scale-95 xs:scale-100 scale-110 border-0 bg-transparent">
+        <DialogHeader>
+          <DialogTitle className="hidden">{""}</DialogTitle>
+          <DialogDescription className="flex">
+            <div className="relative flex h-56 w-full gap-2 rounded-lg bg-transparent">
+              {/* Image Section */}
+              <div className="relative h-full w-2/5 overflow-hidden rounded-md *:text-start">
+                <Image
+                  // src={`/assets/councils/${image}`}
+                  src={`/lebron.webp`}
+                  alt={`${name}'s Image`}
+                  fill
+                  sizes="33%"
+                  className="pointer-events-none object-cover"
+                />
+                <div className="absolute bottom-4 left-4 z-10 lg:bottom-2 lg:left-2">
+                  <h1 className="sm:text-lg font-bold text-white lg:hidden">{role}</h1>
+                  <h1 className="font-medium text-white text-sm sm:text-base lg:text-lg lg:font-bold">{name}</h1>
+                </div>
+                <div className="bg-gradient-from-transparent absolute inset-x-0 bottom-0 h-20 bg-gradient-to-t from-black to-transparent" />
+              </div>
 
-        <hr className="border-gray-light my-2 w-full border-b" />
-
-        <h3 className="text-sm">Past Experience</h3>
-        <ul className="mt-2 flex flex-wrap gap-2 text-xs text-white">
-          {experiences.map((experience, index) => (
-            <li key={index} className="rounded-md border border-white px-2 py-1">
-              {experience}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+              <div className="bg-gray flex w-3/5 flex-col justify-start rounded-md p-2 2xs:p-4">
+                <h3 className="self-start text-white">Past Experience</h3>
+                <ul className="mt-3 flex flex-wrap gap-2 text-[10px] xs:text-xs text-white">
+                  {experiences.map((experience, index) => (
+                    <li
+                      key={index}
+                      className="rounded-full border border-neutral-500 bg-neutral-700/50 px-2.5 py-1"
+                    >
+                      {experience}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 };
