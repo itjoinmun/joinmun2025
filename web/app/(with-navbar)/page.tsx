94a02a915e849f114ac0c96da8b469e1a31c930b
Andre <andhiki28@gmail.com>
2025-05-10 10:50:49 +0700

    fix(landing page): show pricing section

--- web/app/(with-navbar)/page.tsx
+++ web/app/(with-navbar)/page.tsx
@@ -6,7 +6,7 @@ import WelcomingRemarks from "@/modules/welcoming-remarks";
 import Events from "@/modules/home/events";
 import Councils from "@/modules/home/councils";
 import ContactUs from "@/modules/contact-us";
-// import Pricing from "@/modules/home/pricing";
+import Pricing from "@/modules/home/pricing";
 
 export default function Home() {
   return (
@@ -19,7 +19,7 @@ export default function Home() {
       <Events />
       <Councils />
       <ContactUs />
-      {/* <Pricing /> */}
+      <Pricing />
     </>
   );
 }
