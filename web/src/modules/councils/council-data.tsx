6ae8b647d3848143760c1dc803f7f40461b5d5fb
Andre <andhiki28@gmail.com>
2025-05-15 20:57:22 +0700

    fix(build)

--- web/src/modules/councils/council-data.tsx
+++ web/src/modules/councils/council-data.tsx
@@ -1,12 +1,12 @@
 import { Heading } from "@/components/Layout/section-heading";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-
 import { Council } from "@/utils/helpers/councils";
 import ReadMoreCouncil from "./read-more";
+import { cn } from "@/utils/helpers/cn";
 
 const CouncilData = (props: Council) => {
   return (
-    <section className="bg-gray flex gap-6 p-4 md:items-center md:p-6 rounded-lg">
+    <section className="bg-gray flex gap-6 rounded-lg p-4 md:items-center md:p-6">
       <div className="flex w-full flex-col gap-2">
         <Heading className="text-gradient-gold flex items-center gap-3 text-2xl font-bold text-pretty">
           <Avatar className="size-16 md:size-24">
@@ -18,17 +18,19 @@ const CouncilData = (props: Council) => {
 
         <hr className="border-gray-light my-2 w-full border-b" />
 
-        <div className="flex flex-col gap-3 *:text-pretty md:gap-6">
-          <div className="space-y-4">
-            <p className="font-bold">Topic:</p>
-            <ReadMoreCouncil
-              html={props.topic}
-              limit={300}
-              className="w-full text-sm md:text-base"
-            />
-          </div>
+        <div className={cn(props.topic && "flex flex-col gap-3 *:text-pretty md:gap-6")}>
+          {props.topic && (
+            <div className="space-y-4">
+              <p className="font-bold">Topic:</p>
+              <ReadMoreCouncil
+                html={props.topic}
+                limit={300}
+                className="w-full text-sm md:text-base"
+              />
+            </div>
+          )}
 
-          <hr className="border-gray-light my-2 w-full border-b" />
+          {props.topic && <hr className="border-gray-light my-2 w-full border-b" />}
 
           <div className="space-y-4">
             <p className="font-bold">Council Explanation:</p>
