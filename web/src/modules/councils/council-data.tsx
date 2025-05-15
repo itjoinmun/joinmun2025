b84951810ea7df694a11b9df41fd00a380cde923
Andre <andhiki28@gmail.com>
2025-05-15 20:43:17 +0700

    fix(council): fix copyweriting in council page

--- web/src/modules/councils/council-data.tsx
+++ web/src/modules/councils/council-data.tsx
@@ -1,29 +1,43 @@
 import { Heading } from "@/components/Layout/section-heading";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 import { Council } from "@/utils/helpers/councils";
+import ReadMoreCouncil from "./read-more";
 
 const CouncilData = (props: Council) => {
   return (
-    <section className="bg-gray flex gap-6 p-4 md:items-center md:p-6">
-      <Avatar className="hidden size-24 md:flex">
-        <AvatarImage src={`/assets/councils/logo/${props.logo}`} />
-        <AvatarFallback>{props.name}</AvatarFallback>
-      </Avatar>
+    <section className="bg-gray flex gap-6 p-4 md:items-center md:p-6 rounded-lg">
       <div className="flex w-full flex-col gap-2">
         <Heading className="text-gradient-gold flex items-center gap-3 text-2xl font-bold text-pretty">
-          <Avatar className="size-16 md:hidden">
-            <AvatarImage src={`/assets/councils/${props.src}`} />
+          <Avatar className="size-16 md:size-24">
+            <AvatarImage src={`/assets/councils/logo/${props.logo}`} />
             <AvatarFallback>{props.name}</AvatarFallback>
           </Avatar>
-
-          {props.fullname}
+          <span className="ml-2 md:text-4xl/snug xl:ml-5">{props.fullname}</span>
         </Heading>
 
         <hr className="border-gray-light my-2 w-full border-b" />
 
-        <div className="flex flex-col gap-3 *:text-pretty md:flex-row md:gap-6">
-          <p className="w-full font-bold md:w-2/6 md:text-lg">&quot;{props.quote}&quot;</p>
-          <p className="w-full text-sm md:w-4/6 md:text-base">{props.description}</p>
+        <div className="flex flex-col gap-3 *:text-pretty md:gap-6">
+          <div className="space-y-4">
+            <p className="font-bold">Topic:</p>
+            <ReadMoreCouncil
+              html={props.topic}
+              limit={300}
+              className="w-full text-sm md:text-base"
+            />
+          </div>
+
+          <hr className="border-gray-light my-2 w-full border-b" />
+
+          <div className="space-y-4">
+            <p className="font-bold">Council Explanation:</p>
+            <ReadMoreCouncil
+              html={props.explanation}
+              limit={300}
+              className="w-full text-sm font-medium md:text-base"
+            />
+          </div>
         </div>
       </div>
     </section>
