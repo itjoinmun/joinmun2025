d8d5591f1ad757174abcdd06d30823d7d2bba816
Devin <sultandevinosuyudi@mail.ugm.ac.id>
2025-06-10 12:26:16 +0700

    fix(registration): remove unused import

--- web/src/modules/dashboard/delegates/registration/registration-nav.tsx
+++ web/src/modules/dashboard/delegates/registration/registration-nav.tsx
@@ -4,7 +4,6 @@ import { useFormStatus } from "@/utils/hooks/use-form-status";
 import { ChevronLeft } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import path from "path";
 
 const RegistrationNav = ({ onSubmit }: { onSubmit?: () => void }) => {
   const { submitting } = useFormStatus();
