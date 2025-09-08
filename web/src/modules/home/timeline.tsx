c346910d3418065c4f87c253b5a8216a7879c346
JOINMUN 2025 <eventjoinmun2025@gmail.com>
2025-09-08 21:36:19 +0700

    Update timeline.tsx

--- web/src/modules/home/timeline.tsx
+++ web/src/modules/home/timeline.tsx
@@ -14,15 +14,15 @@ import { fadeInVariants, slideInItemVariants } from "@/utils/helpers/animation-v
 
 const TIMELINE_CONTENT = [
   {
-    date: "16 June - 14 July",
+    date: "16 Jun - 14 Jul",
     title: "Early Bird Registration",
   },
   {
-    date: "29 July - 24 August",
+    date: "29 Jul - 24 Aug",
     title: "Regular Wave Registration",
   },
   {
-    date: "Coming Soon",
+    date: "8 Sep - 29 Sep",
     title: "Late Wave Registration",
   },
   {
