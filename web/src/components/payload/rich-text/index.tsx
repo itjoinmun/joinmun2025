4e973509cd748ff95b38e1e8a068751efe42a110
sultandevin <sultan.suyudi@gmail.com>
2025-10-05 22:21:56 +0700

    fix: move to css file for rich text styling

--- web/src/components/payload/rich-text/index.tsx
+++ web/src/components/payload/rich-text/index.tsx
@@ -1,6 +1,7 @@
 import { RichText as RichTextConverter } from "@payloadcms/richtext-lexical/react";
 import { SerializedEditorState } from "@payloadcms/richtext-lexical/lexical";
 import { jsxConverter } from "./converters";
+import "@/styles/rich.css"
 
 type Props = {
   data: SerializedEditorState;
@@ -9,5 +10,10 @@ type Props = {
 export function RichText(props: Props) {
   const { className, ...rest } = props;
 
-  return <RichTextConverter className={className} {...rest} converters={jsxConverter} />;
+  return (
+
+    <RichTextConverter className={'rich'} {...rest} converters={jsxConverter} />
+  )
+
+>>>>>>> 415dd0f (fix: move to css file for rich text styling)
 }
