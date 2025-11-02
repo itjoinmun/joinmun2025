1c75f759048582f0305b35321b675d63eaeaa00e
Andre <andhiki28@gmail.com>
2025-11-02 07:56:33 +0700

    fix: home articles

--- web/src/modules/home/home-articles.tsx
+++ web/src/modules/home/home-articles.tsx
@@ -2,6 +2,7 @@ import Container from "@/components/ui/container";
 import config from "@payload-config";
 import { getPayload } from "payload";
 import ArticleCard from "../articles/article-card";
+import { ClockFadingIcon } from "lucide-react";
 
 const HomeArticles = async () => {
   const payload = await getPayload({ config });
@@ -21,7 +22,12 @@ const HomeArticles = async () => {
     <Container className="py-0">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
         {articles.length > 0 ? (
-            articles.map((article) => <ArticleCard key={article.id} {...article} />)
+          articles.map((article) => <ArticleCard key={article.id} {...article} />)
+        ) : (
+          <div className="text-accent col-span-full flex min-h-80 flex-col items-center justify-center gap-4 text-center">
+            <ClockFadingIcon className={`text-accent col size-16`} />
+            <p className="text-pretty">Stay tuned. The articles will be published soon!</p>
+          </div>
         )}
       </div>
     </Container>
