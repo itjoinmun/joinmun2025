a958c3e21960e0ffbc4b15de37f302c628a79be3
JOINMUN 2025 <eventjoinmun2025@gmail.com>
2025-11-02 07:46:44 +0700

    Update home-articles.tsx

--- web/src/modules/home/home-articles.tsx
+++ web/src/modules/home/home-articles.tsx
@@ -23,20 +23,8 @@ const HomeArticles = async () => {
   return (
     <Container className="py-0">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
-        {isReveal ? (
-          articles.length > 0 ? (
+        {articles.length > 0 ? (
             articles.map((article) => <ArticleCard key={article.id} {...article} />)
-          ) : (
-            <div className="text-accent col-span-full flex min-h-80 flex-col items-center justify-center gap-4 text-center">
-              <ClockFadingIcon className={`text-accent col size-16`} />
-              <p className="text-pretty">Stay tuned. The articles will be published soon!</p>
-            </div>
-          )
-        ) : (
-          <div className="text-accent col-span-full flex min-h-80 flex-col items-center justify-center gap-4 text-center">
-            <ClockFadingIcon className={`text-accent col size-16`} />
-            <p className="text-pretty">Stay tuned. The articles will be published soon!</p>
-          </div>
         )}
       </div>
     </Container>
